def843cbe951e07e7039c33e90920bd454a96062
Shuto-Takahshi <shuto.takahashi.124@gmail.com>
2020-09-11 15:28:48 +0900

    マルチログイン　モデルとマイグレーションファイル

---
+++ app/Admin.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function photos()
+    {
+        return $this->hasMany('App\Photo');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
+    }
+
+    public function isFollowedBy(?User $user)
+    {
+        return $user
+            ? (bool)$this->followers->where('id', $user->id)->count()
+            : false;
+    }
+
+    public function getCountFollowersAttribute()
+    {
+        return $this->followers->count();
+    }
+
+    public function getCountFollowingsAttribute()
+    {
+        return $this->followings->count();
+    }
+
+    public static $rules = [
+        'user_name' => 'required',
+    ];
+}
